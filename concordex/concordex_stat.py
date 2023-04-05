f9f283e669e219d521907c3a6195af4f039fb533
Sina Booeshaghi <sbooeshaghi@gmail.com>
2023-04-05 12:45:49 -0700

    clean up files, fix naming

--- concordex/concordex_stat.py
+++ concordex/concordex_stat.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import numpy as np
+from scipy.io import mmread
+import random
 
-from .concordex_map import concordex_map
+from .nomap_map import nomap_map
 
 
 def setup_trace_args(parser):
@@ -39,28 +41,30 @@ def validate_trace_args(parser, args):
 
 def run_trace(mtx_fname, assignments_fname, output):
     mtx = mmread(mtx_fname)
-    assignments = pd.read_csv(assignments_fname, header=None)
-    trace, random_trace, corrected_trace = concordex_trace(mtx, assignments)
+    assignments = pd.read_csv(assignments_fname, header = None)
+    assignments.columns = ['label']
+    assignments = assignments['label'].values
+    trace, random_trace, corrected_trace = nomap_trace(mtx, assignments)
     # Missing format for output file containing trace values
+    print(f'Trace: {trace}')
+    print(f'Average random trace: {random_trace}')
+    print(f'Corrected trace: {corrected_trace}')
     return
 
 
-def concordex_trace(mtx, assignments):
+def nomap_trace(mtx, assignments):
     # mapped matrix with normal assignments
-    map_mtx = concordex_map(mtx, assignments)
+    map_mtx = nomap_map(mtx, assignments)
 
     # mapped matrix with permuted assignments
-    n_iters = 50
+    n_iters = 15  
     random_map_matrices = []
     for i in range(n_iters):
-        random_map_matrices.append(concordex_map(mtx, random.shuffle(assignments)))
+      random.shuffle(assignments)
+      random_map_matrices.append(nomap_map(mtx, assignments))
 
     # compute trace and random
-    trace = np.trace(map_mtx) / map_mtx.shape[0]
-    random_trace = [
-        np.trace(random_map_mtx) / random_map_mtx.shape[0]
-        for random_map_mtx in random_map_matrices
-    ]
+    trace = np.trace(map_mtx)/map_mtx.shape[0]
+    random_trace = np.mean([np.trace(random_map_mtx)/random_map_mtx.shape[0] for random_map_mtx in random_map_matrices])
     corrected_trace = trace / random_trace
-
     return trace, random_trace, corrected_trace
