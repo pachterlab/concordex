e90f7ebf2ca8ffe77b9b8eae3cd728c5e3a3a07f
Alexandra Kim <alexandrasuriya@gmail.com>
2024-01-23 14:06:51 -0800

    Changed descriptors with KNN graph

--- concordex/concordex_map.py
+++ concordex/concordex_map.py
@@ -9,7 +9,7 @@ def setup_map_args(parser):
         description="",
         help="",
     )
-    parser_map.add_argument("mtx_file", help="KNN matrix")
+    parser_map.add_argument("knn_file", help="Path to KNN graph")
     parser_map.add_argument(
         "-a",
         metavar="Labels",
@@ -36,31 +36,31 @@ def setup_map_args(parser):
 
 
 def validate_map_args(parser, args):
-    mtx_fname = args.mtx_file
+    knn_fname = args.knn_file
     labels_fname = args.a
     output = args.o
     neighbors = args.n
-    run_map(mtx_fname, labels_fname, neighbors, output)
+    run_map(knn_fname, labels_fname, neighbors, output)
     return
 
 
-def run_map(mtx_fname, labels_fname, neighbors, output):
-    mtx = mmread(mtx_fname)
+def run_map(knn_fname, labels_fname, neighbors, output):
+    knn = mmread(knn_fname)
     labels = pd.read_csv(labels_fname, header=None)
     labels.columns = ["label"]
     labels = labels["label"].values
-    map_mtx = concordex_map(mtx, labels)
-    map_mtx.to_csv(output, sep="\t")
+    map_knn = concordex_map(knn, labels)
+    map_knn.to_csv(output, sep="\t")
     return
 
 
-def concordex_map(mtx, labels):
+def concordex_map(x, labels):
     """
-    @param mtx: A numeric matrix specifying the neighborhood structure of observations. Typically an adjacency matrix produced by a k-Nearest Neighbor algorithm. It can also be a matrix whose rows correspond to each observation and columns correspond to neighbor indices, i.e. matrix form of an adjacency list which can be a matrix due to fixed number of neighbors.
+    @param x: A numeric matrix specifying the neighborhood structure of observations. Typically an adjacency matrix produced by a k-Nearest Neighbor algorithm. It can also be a matrix whose rows correspond to each observation and columns correspond to neighbor indices, i.e. matrix form of an adjacency list which can be a matrix due to fixed number of neighbors.
     
     @param labels: A numeric or character vector containing the label or class corresponding to each observation. For example, a cell type or cluster ID.
     """
-    df = pd.DataFrame(conn.A, index=labels, columns=labels)
+    df = pd.DataFrame(x, index=labels, columns=labels)
     t = df.T.groupby(df.columns).sum().T.groupby(df.columns).sum()
     map_mtx = t.div(t.sum(1), axis=0)
     return map_mtx
