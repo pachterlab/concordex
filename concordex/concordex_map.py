fb8fcba5ece06d9dbaf9e58e4467ac3a24b612ac
Alexandra Kim <alexandrasuriya@gmail.com>
2024-01-23 14:22:59 -0800

    Started check_graph()

--- concordex/concordex_map.py
+++ concordex/concordex_map.py
@@ -1,3 +1,5 @@
+import warnings
+import numpy as np
 import pandas as pd
 from scipy.io import mmread
 from sklearn.neighbors import kneighbors_graph
@@ -34,6 +36,39 @@ def setup_map_args(parser):
     # default value is false
     return parser_map
 
+def check_graph(graph, neighbors):
+    orientation = check_matrix_dims(graph, neighbors, return_dims=False)
+    graph = reorient_matrix(graph, neighbors, how=orientation)
+    
+    diag_sum = np.trace(graph)
+    if diag_sum != 0:
+        warnings.warn("Some nodes in the graph are self-referential. There should not be an edge between a node and itself.")
+    
+    return graph
+
+def check_matrix_dims(x, k):
+    dims = np.shape(x)
+    
+    def guess_orientation(x, k, dims):
+        if np.diff(dims) == 0:
+            if np.all(np.sum(x, axis=1) / k == 1):
+                return 1
+            if np.all(np.sum(x, axis=0) / k == 1):
+                return 2
+        else:
+            axis = np.where(dims == k)[0]
+            if len(axis) == 0:
+                return None
+            if axis == 0:
+                return 3
+            if axis == 1:
+                return 4
+
+        return None
+
+def reorient_matrix():
+    pass
+    
 
 def validate_map_args(parser, args):
     knn_fname = args.knn_file
@@ -49,12 +84,12 @@ def run_map(knn_fname, labels_fname, neighbors, output):
     labels = pd.read_csv(labels_fname, header=None)
     labels.columns = ["label"]
     labels = labels["label"].values
-    map_knn = concordex_map(knn, labels)
+    map_knn = concordex_map(knn, labels, neighbors)
     map_knn.to_csv(output, sep="\t")
     return
 
 
-def concordex_map(x, labels):
+def concordex_map(x, labels, neighbors):
     """
     @param x: A numeric matrix specifying the neighborhood structure of observations. Typically an adjacency matrix produced by a k-Nearest Neighbor algorithm. It can also be a matrix whose rows correspond to each observation and columns correspond to neighbor indices, i.e. matrix form of an adjacency list which can be a matrix due to fixed number of neighbors.
     
